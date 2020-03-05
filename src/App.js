515554a71867e7a42545f881974e5a82792e4a0c
Victor Malov <victor.malov@breeffy.com>
2020-03-05 18:31:50 +0300

    app component changed to be class-based

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return <div></div>;
+  }
 }
 
 export default App;
