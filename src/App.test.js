54c366fa28860b725021ad8d57d9a50767d147cb
Victor Malov <victor.malov@breeffy.com>
2020-03-05 16:50:33 +0300

    change test name

--- src/App.test.js
+++ src/App.test.js
@@ -5,7 +5,6 @@ import EnzymeAdapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
-test('renders learn react link', () => {
+test('renders without crashing', () => {
   const wrapper = shallow(<App />);
-  console.log(wrapper.debug());
 });
