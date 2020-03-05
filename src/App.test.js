925282b2c43c95b233c526dc4fb0886fd2383daf
Victor Malov <victor.malov@breeffy.com>
2020-03-05 21:26:09 +0300

    add first test

--- src/App.test.js
+++ src/App.test.js
@@ -5,7 +5,11 @@ import EnzymeAdapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
-test('renders without error', () => {});
+test('renders without error', () => {
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find('[data-test="component-app"]');
+  expect(appComponent.length).toBe(1);
+});
 
 test('renders increment button', () => {});
 
